729a4e476e155d2cfa957a66f64d4cd67803bd3a
Alex Cotiugă <alexandru.cotiuga@xwiki.com>
2018-01-01 14:09:44 +0200

    Create a script to clone all the Paid apps + Licensing #2 * provide current location as default location, if the user doesn't specify any

--- cloneApps.sh
+++ cloneApps.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
+CURRENT_LOCATION=`pwd`
 echo -e "\033[0;32mWhere should the Paid apps be cloned? (e.g.: /home/user/Git/PaidApps)\033[0m"
 appsListFile=`find ~/ -name 'apps-list.txt'`
-read -e -p "> " location
+read -e -p "> (${CURRENT_LOCATION}): " location
 if [[ $location ]]; then
-  cd $location
-  while read p; do
-    git clone git@github.com:xwikisas/${p}.git
-  done <$appsListFile
-else
-  echo "Please specify the location!"
+  CURRENT_LOCATION=$location
 fi
+cd $CURRENT_LOCATION
+while read p; do
+  git clone git@github.com:xwikisas/${p}.git
+done <$appsListFile
