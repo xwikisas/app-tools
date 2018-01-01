11719566264eac38550ebd95edf6c23350c05dbe
Alex Cotiugă <alexandru.cotiuga@xwiki.com>
2018-01-01 13:51:05 +0200

    Create a script to clone all the Paid apps + Licensing #2 * the script to clone all the apps in a directory specified by the user * the list of all current paid app + licensing, that needs to be cloned

---
+++ cloneApps.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo -e "\033[0;32mWhere should the Paid apps be cloned? (e.g.: /home/user/Git/PaidApps)\033[0m"
+appsListFile=`find ~/ -name 'apps-list.txt'`
+read -e -p "> " location
+if [[ $location ]]; then
+  cd $location
+  while read p; do
+   git clone git@github.com:xwikisas/${p}.git
+  done <$appsListFile
+else
+  echo "Please specify the location!"
+fi
