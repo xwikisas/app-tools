d5c5fb1ac9def1c6b79976f6dc311a3634e68a92
Alex Cotiugă <alexandru.cotiuga@xwiki.com>
2018-05-08 16:07:57 +0300

    Fix clone script

--- clone-apps.sh
+++ clone-apps.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 CURRENT_LOCATION=`pwd`
 echo -e "\033[0;32mWhere should the Paid apps be cloned? (e.g.: /home/user/Git/PaidApps)\033[0m"
-appsListFile=`find ~/ -name 'apps-list.txt'`
+appsListFile=`find . -name 'apps-list.txt'`
 read -e -p "> (${CURRENT_LOCATION}): " location
 if [[ $location ]]; then
   CURRENT_LOCATION=$location
